0ed04f06f25d32ee2551b0ca7fbb12f6b19d0e5f
buith <buith@DESKTOP-O657O8L>
2024-11-20 14:50:19 +0700

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student> { };
+            Student student = new Student()
+            {
+                Id = 1,
+                Name = "ngoc",
+                Age = 6
+            };
+            students.Add(student);
+            Student student1 = new Student()
+            {
+                Id = 2,
+                Name = "mai",
+                Age = 9
+            };
+            students.Add(student1);
+            Student student2 = new Student()
+            {
+                Id = 3,
+                Name = "linh",
+                Age = 9
+            };
+            students.Add(student2);
+            Student student3 = new Student()
+            {
+                Id = 4,
+                Name = "hien",
+                Age = 16
+            };
+            students.Add(student3);
+            Student student4 = new Student()
+            {
+                Id = 5,
+                Name = "Anh",
+                Age = 20
+            };
+            students.Add(student4);
+            Console.WriteLine("Danh sach toan bo sinh vien:");
+            foreach (var stud in students)
+            {
+                stud.showThongTin();
+            }
+            var s1 = students.Where(x => x.Age >= 15 && x.Age <= 18).ToList();
+            Console.WriteLine("\nDanh sach sinh vien tu 15 tuoi tro len:");
+            foreach (var s in s1)
+            {
+                s.showThongTin();
+            }
+
+            var s2 = students.Where(s => s.Name.StartsWith("A")).ToList();
+            Console.WriteLine("\nSinh vien co ten bat dau bang chu A:");
+                foreach (var s in s2)
+                {
+                    s.showThongTin();
+                }
+            int s3 = students.Sum(s => s.Age);
+            Console.WriteLine($"Tong tuoi cua cac sinh vien: {s3}");
+            var s4 = students.OrderByDescending(s => s.Age).FirstOrDefault();
+                Console.WriteLine($"Sinh vien co tuoi lon nhat: ");
+                Console.WriteLine($"ID: {s4.Id}, Name: {s4.Name}, Age: {s4.Age}");
+            var s5 = students.OrderBy(s => s.Age).ToList();
+            Console.WriteLine("Danh sach sinh vien tuoi tang dan:");
+            foreach (var s in s5)
+            {
+                Console.WriteLine($"ID: {s.Id}, Name: {s.Name}, Age: {s.Age}");
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ ConsoleApp2/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Student
+    {
+        private int id;
+        private string name;
+        private int age;
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public void showThongTin()
+        {
+            Console.WriteLine(""+Id+ " - "+ Name + " - " + Age + "");
+        }
+    }
+}
